fb284f5a9aa36971a53da2f84b75b4c25142cc8c
mizuhara123 <yuchengbo875@gmail.com>
2025-10-24 22:34:03 +0900

    Update and rename upload.js to 0

--- src/utils/src/utils/upload.js
+++
@@ -1,8 +0,0 @@
-const BASE = 'https://43.165.181.179:9090'
-
-export const UPLOAD_URL = `${BASE}/files/upload`
-
-export function normalizeFileUrl(url) {
-  if (!url) return url
-  return url.replace(/^http:\/\/(localhost:9090|43\.165\.181\.179:9090)/, 'https://43.165.181.179:9090')
-}
