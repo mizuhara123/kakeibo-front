4efff471d0736a89842d0776ec57e4610a83bd2b
mizuhara123 <yuchengbo875@gmail.com>
2025-10-24 22:02:05 +0900

    Update User.vue

--- src/views/manager/User.vue
+++ src/views/manager/User.vue
@@ -93,7 +93,7 @@
         <el-form-item label="アバター">
    <el-upload
   class="avatar-uploader"
-  action="$baseUrl + '/files/upload'"
+  :action="'https://43.165.181.179:9090/files/upload'"
   :headers="{ token: user.token }"
   list-type="picture"
   :on-success="handleAvatarSuccess"
