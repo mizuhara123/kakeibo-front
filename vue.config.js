d505037d7357392f53ed5107e23084c339fa7626
mizuhara123 <yuchengbo875@gmail.com>
2025-10-24 18:46:05 +0900

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,4 @@
-const { defineConfig } = require('@vue/cli-service')
-const path = require('path')
+const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
@@ -8,16 +7,11 @@ module.exports = defineConfig({
   },
   chainWebpack: config => {
     config.plugin('html').tap(args => {
-      args[0].title = '家計簿システム'
-      return args
-    })
-
-    // ✅ 这段是关键：确保 public/_redirects 被复制进 dist/
-    config.plugin('copy').tap(args => {
-      args[0][0].from = path.resolve(__dirname, 'public')
-      args[0][0].to = path.resolve(__dirname, 'dist')
-      return args
-    })
+      if (args && args[0]) {
+        args[0].title = '管理系統';
+      }
+      return args;
+    });
   }
-})
+});
 
