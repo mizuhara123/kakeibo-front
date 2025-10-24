b0a275cc62802e5ee9ef6b3e02b890ca83999ecd
mizuhara123 <yuchengbo875@gmail.com>
2025-10-24 22:41:06 +0900

    Update upload.js

--- src/utils/src/utils/upload.js
+++ src/utils/src/utils/upload.js
@@ -4,5 +4,5 @@ export const UPLOAD_URL = `${BASE}/files/upload`
 
 export function normalizeFileUrl(url) {
   if (!url) return url
-  return url.replace(/^http:\/\/(localhost:9090|43\.165\.181\.179:9090)/, 'https://43.165.181.179:9090')
+  return url.replace(/^http:\/\/(localhost:9090|43\.165\.181\.179:9090)/, 'https://43.165.181.179')
 }
